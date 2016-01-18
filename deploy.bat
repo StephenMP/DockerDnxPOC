6e84e3d269e643698277fd081be22698ecd9ae0e
Stephen Porter <sporter@scentsy.com>
2016-01-18 14:23:02 -0700

    Added another value

--- deploy.bat
+++ deploy.bat
@@ -1,12 +1,26 @@
-@set /p VERSION= "What version to deploy?  "
-@set /p TARGET= "What stack to target?  "
-@set /p COLOR= "What color to deploy?  "
+@echo off
 
-@echo Setting %TARGET%-%COLOR% to tag %VERSION%
-@tutum service set --image tutum.co/scenpoc/dnx-poc:%VERSION% --redeploy --sync %TARGET%-%COLOR%
+echo ==================================================
+echo Beginning Deployment
+echo --------------------------------------------------
 
-@echo Resting for a minute
-@sleep 60
+: Get necessary data for what to deploy
+set /p VERSION= "What version to deploy?  "
+set /p TARGET= "What stack to target?  "
+set /p COLOR= "What color to deploy?  "
 
-@echo Linking load balancer to %TARGET%-%COLOR%
-@tutum service set --link %TARGET%-%COLOR%:%TARGET%-%COLOR% %TARGET%-lb
+: Tag the target service with the image version we want
+echo Setting %TARGET%-%COLOR% to version %VERSION%
+tutum service set --image tutum.co/scenpoc/dnx-%TARGET%:%VERSION% --redeploy --sync %TARGET%-%COLOR%
+
+: Wait for 5 minutes for the service to fully stand up
+echo Waiting for the Service to Mature
+timeout /t 300
+
+: Link the load balancer to the updated service
+echo Linking load balancer to %TARGET%-%COLOR%
+tutum service set --link %TARGET%-%COLOR%:%TARGET%-%COLOR% %TARGET%-lb
+
+: All done :)
+echo Deployment Complete!
+echo ==================================================
